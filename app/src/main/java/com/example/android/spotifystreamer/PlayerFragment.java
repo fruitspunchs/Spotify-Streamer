1d6388c7495868cc54d34bdd91db2a097be7cbf4
fruitspunchs <zargon234@gmail.com>
2015-07-10 00:56:44 +0800

    feat: add track player class

---
+++ app/src/main/java/com/example/android/spotifystreamer/PlayerFragment.java
@@ -0,0 +1,30 @@
+package com.example.android.spotifystreamer;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+/**
+ * Created by User on 7/10/2015.
+ */
+public class PlayerFragment extends DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_player, container, false);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
+}
