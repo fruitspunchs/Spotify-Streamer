0323271a9b6e8f06bc2459d0f0fe4dd288277aef
fruitspunchs <zargon234@gmail.com>
2015-07-10 21:32:05 +0800

    feat: implement parcleable

--- app/src/main/java/com/example/android/spotifystreamer/TrackInfo.java
+++ app/src/main/java/com/example/android/spotifystreamer/TrackInfo.java
@@ -1,17 +1,51 @@
 package com.example.android.spotifystreamer;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
 /**
  * Stores track information
  */
-public class TrackInfo {
+public class TrackInfo implements Parcelable {
+    public static final Parcelable.Creator<TrackInfo> CREATOR
+            = new Parcelable.Creator<TrackInfo>() {
+        public TrackInfo createFromParcel(Parcel in) {
+            return new TrackInfo(in);
+        }
+
+        public TrackInfo[] newArray(int size) {
+            return new TrackInfo[size];
+        }
+    };
     private final ArrayList<String> mTrackNames = new ArrayList<>();
     private final ArrayList<String> mAlbumNames = new ArrayList<>();
     private final ArrayList<String> mLargeThumbnails = new ArrayList<>();
     private final ArrayList<String> mMediumThumbnails = new ArrayList<>();
     private final ArrayList<String> mTrackPreviewUrls = new ArrayList<>();
 
+    private TrackInfo(Parcel in) {
+        in.readStringList(mTrackNames);
+        in.readStringList(mAlbumNames);
+        in.readStringList(mLargeThumbnails);
+        in.readStringList(mMediumThumbnails);
+        in.readStringList(mTrackPreviewUrls);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringList(mTrackNames);
+        dest.writeStringList(mAlbumNames);
+        dest.writeStringList(mLargeThumbnails);
+        dest.writeStringList(mMediumThumbnails);
+        dest.writeStringList(mTrackPreviewUrls);
+    }
 
     public ArrayList<String> getTrackPreviewUrls() {
         return mTrackPreviewUrls;
