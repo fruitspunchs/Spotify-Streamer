0989f16308e66b0d088a9e0b547261f5850b7d7d
fruitspunchs <zargon234@gmail.com>
2015-07-12 13:22:57 +0800

    feat: add displayToast utility method

--- app/src/main/java/com/example/android/spotifystreamer/Utility.java
+++ app/src/main/java/com/example/android/spotifystreamer/Utility.java
@@ -1,17 +1,28 @@
 package com.example.android.spotifystreamer;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.widget.Toast;
 
 import java.util.Random;
 
 /**
- * Created by User on 7/10/2015.
+ * Helper classes
  */
 public class Utility {
     private static Random randomNumber = new Random();
 
     public static ColorDrawable randomColorDrawable() {
         return new ColorDrawable(Color.rgb(randomNumber.nextInt(256), randomNumber.nextInt(256), randomNumber.nextInt(256)));
+    }
+
+    public static void displayToast(Context context, Toast toast, String message) {
+        if (toast != null) {
+            toast.cancel();
+        }
+
+        toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
